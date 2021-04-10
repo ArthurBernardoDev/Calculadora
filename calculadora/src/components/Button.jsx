09614810ecfd168babfad933f07812d5c33b1ba0
Arthur <arthurutschgg@gmail.com>
2021-04-09 23:16:11 -0300

    Funcionalidade

--- calculadora/src/components/Button.jsx
+++ calculadora/src/components/Button.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
 import './Button.css'
 
-export default props => 
-<button className="Button">{props.label}</button> 
-    
+export default props => {
+    let classes = 'Button '
+    classes += props.operation ? 'operation' : ''
+    classes += props.double ? 'double' : ''
+    classes += props.triple ? 'triple' : ''
+
+    return (
+        <button 
+            onClick={e => props.click && props.click(props.label)}
+            className={classes}>
+            {props.label}
+        </button>
+    ) 
+}
